fb0718cfc454919f61c36ed0218f4f2f59b0d15e
Felipe <felipe.ferreira1003@gmail.com>
2021-04-30 14:56:47 -0300

    Insert a demo mensage to test LCD - test ok

--- src/main.c
+++ src/main.c
@@ -5,20 +5,23 @@
 *   MAIN FILE
 */
 
-sbit LCD_RS at RB0_bit;
-sbit LCD_EN at RB1_bit;
-sbit LCD_D4 at RB2_bit;
-sbit LCD_D5 at RB3_bit;
-sbit LCD_D6 at RB4_bit;
-sbit LCD_D7 at RB5_bit;
+sbit LCD_RS at RD0_bit;
+sbit LCD_EN at RD1_bit;
+sbit LCD_D4 at RD2_bit;
+sbit LCD_D5 at RD3_bit;
+sbit LCD_D6 at RD4_bit;
+sbit LCD_D7 at RD5_bit;
 
-sbit LCD_RS_Direction at TRISB0_bit;
-sbit LCD_EN_Direction at TRISB1_bit;
-sbit LCD_D4_Direction at TRISB2_bit;
-sbit LCD_D5_Direction at TRISB3_bit;
-sbit LCD_D6_Direction at TRISB4_bit;
-sbit LCD_D7_Direction at TRISB5_bit;
+sbit LCD_RS_Direction at TRISD0_bit;
+sbit LCD_EN_Direction at TRISD1_bit;
+sbit LCD_D4_Direction at TRISD2_bit;
+sbit LCD_D5_Direction at TRISD3_bit;
+sbit LCD_D6_Direction at TRISD4_bit;
+sbit LCD_D7_Direction at TRISD5_bit;
 
+
+char msnInicial [21] = "---- JC MODULOS ----";
+char tracos [21]     = "--------------------";
 
 void InitializeSystem(void){
     TRISA = 0b00000000;
@@ -30,5 +33,13 @@ void InitializeSystem(void){
 
 void main(void){
     InitializeSystem();
+    
+    Lcd_Out(1,1,tracos);
+    Lcd_Out(2,1,msnInicial);
+    Lcd_Out(3,1,msnInicial);
+    Lcd_Out(4,1,tracos);
 
+    while(1){
+    
+    }
 }
