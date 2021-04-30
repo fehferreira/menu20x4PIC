d980724f6645fa70898a8be3c848d291d3117130
Felipe <felipe.ferreira1003@gmail.com>
2021-04-30 14:47:26 -0300

    Insert LCD library and config LCD on simulator

--- src/main.asm
+++ src/main.asm
@@ -1,22 +1,24 @@
 
 _InitializeSystem:
 
-;main.c,8 :: 		void InitializeSystem(void){
-;main.c,9 :: 		TRISA = 0b00000000;
+;main.c,23 :: 		void InitializeSystem(void){
+;main.c,24 :: 		TRISA = 0b00000000;
 	CLRF        TRISA+0 
-;main.c,10 :: 		TRISB = 0b00000000;
+;main.c,25 :: 		TRISB = 0b00000000;
 	CLRF        TRISB+0 
-;main.c,12 :: 		}
+;main.c,27 :: 		Lcd_Init();
+	CALL        _Lcd_Init+0, 0
+;main.c,29 :: 		}
 L_end_InitializeSystem:
 	RETURN      0
 ; end of _InitializeSystem
 
 _main:
 
-;main.c,14 :: 		void main(void){
-;main.c,16 :: 		InitializeSystem();
+;main.c,31 :: 		void main(void){
+;main.c,32 :: 		InitializeSystem();
 	CALL        _InitializeSystem+0, 0
-;main.c,18 :: 		}
+;main.c,34 :: 		}
 L_end_main:
 	GOTO        $+0
 ; end of _main
--- src/main.c
+++ src/main.c
@@ -5,14 +5,30 @@
 *   MAIN FILE
 */
 
+sbit LCD_RS at RB0_bit;
+sbit LCD_EN at RB1_bit;
+sbit LCD_D4 at RB2_bit;
+sbit LCD_D5 at RB3_bit;
+sbit LCD_D6 at RB4_bit;
+sbit LCD_D7 at RB5_bit;
+
+sbit LCD_RS_Direction at TRISB0_bit;
+sbit LCD_EN_Direction at TRISB1_bit;
+sbit LCD_D4_Direction at TRISB2_bit;
+sbit LCD_D5_Direction at TRISB3_bit;
+sbit LCD_D6_Direction at TRISB4_bit;
+sbit LCD_D7_Direction at TRISB5_bit;
+
+
 void InitializeSystem(void){
     TRISA = 0b00000000;
     TRISB = 0b00000000;
+    
+    Lcd_Init();
 
 }
 
 void main(void){
-
     InitializeSystem();
 
 }
